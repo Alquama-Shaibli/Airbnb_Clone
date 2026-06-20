026dbad608bcba02e112533f5e131572f049fe1b
alquama-shaibli <alquama.r56@gmail.com>
2026-06-20 16:07:48 +0530

    ModelListingBasic

---
+++ models/listing.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    images: { type: String, set: (v) => v === '' ? "https://unsplash.com/photos/modern-white-house-with-large-windows-overlooking-the-ocean-rXq2vzsuLRg" : v },
+    location: { type: String, required: true },
+    price: { type: Number, required: true },
+    country: { type: String, required: true },
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
