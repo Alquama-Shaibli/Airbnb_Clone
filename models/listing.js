2561a79cd67c3b9120eb8d4b17f6a2edb13d905d
alquama-shaibli <alquama.r56@gmail.com>
2026-06-21 13:21:06 +0530

    updatedFiles

--- models/listing.js
+++ models/listing.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    images: { type: String, set: (v) => v === '' ? "https://unsplash.com/photos/modern-white-house-with-large-windows-overlooking-the-ocean-rXq2vzsuLRg" : v },
+    images: { type: String, default : "https://unsplash.com/photos/modern-white-house-with-large-windows-overlooking-the-ocean-rXq2vzsuLRg", set: (v) => v === '' ? "https://unsplash.com/photos/modern-white-house-with-large-windows-overlooking-the-ocean-rXq2vzsuLRg" : v },
     location: { type: String, required: true },
     price: { type: Number, required: true },
     country: { type: String, required: true },
