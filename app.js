64b04c1b4fd25d32ab465ffb3e1da8dc6631e162
alquama-shaibli <alquama.r56@gmail.com>
2026-06-22 14:10:03 +0530

    Updateroutr

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.get("/listings/:id/edit", async (req, res) => {
   res.render("listings/edit.ejs", { listing });
 });
 
+//upadte Route 
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
 
 // Start server
 app.listen(8080, () => {
