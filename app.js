7b7079b20b2d019388940b301dec255284d02659
alquama-shaibli <alquama.r56@gmail.com>
2026-07-22 23:53:33 +0530

    Error.ejs

--- app.js
+++ app.js
@@ -113,7 +113,9 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let{ statusCode=500, message = "Something went wrong!"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("listings/error.ejs", { message });
+    
+    // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
