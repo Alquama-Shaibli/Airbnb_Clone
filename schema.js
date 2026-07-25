3acaa59070af80d88d7e53f1be61b6579ebd7590
alquama-shaibli <alquama.r56@gmail.com>
2026-07-25 22:36:30 +0530

    Updated

--- schema.js
+++ schema.js
@@ -8,7 +8,7 @@ module.exports.listingSchema = joi.object({
         country: joi.string().required(),
         location: joi.string().required(),
         image: joi.object({ url: joi.string().allow("", null), filename: joi.string().allow("", null) }).allow(null)
-        }.required())
-    });
+    }).required()
+});
 
 
