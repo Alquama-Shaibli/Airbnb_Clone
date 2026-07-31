d29afab07cdd6f8dcb94ae0fea41b3e58a810d77
alquama-shaibli <alquama.r56@gmail.com>
2026-07-31 22:30:03 +0530

    ExpressRoute

---
+++ routes/listing.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const Listing = require('../models/listing.js');
+const Review = require('../models/review.js');
+
+
+
+//Index Route
+router.get("/listings", wrapAsync(async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+}));
+
+//New Route
+router.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//Show Route
+router.get("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id).populate("reviews");
+  res.render("listings/show.ejs", { listing });
+}));
+
+
+//Create Route
+router.post("/listings", validateListing, wrapAsync(async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+}));
+
+//Edit Route
+router.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+}));
+
+//Update Route
+router.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+}));
+
+//Delete Route
+router.delete("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+}));
+
+module.exports = router;
