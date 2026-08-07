9b4fa1c3fd063bc1fa7dbe560f5ce1ca2d103260
alquama-shaibli <alquama.r56@gmail.com>
2026-08-07 21:41:32 +0530

    ErrorFixed

--- models/listing.js
+++ models/listing.js
@@ -12,7 +12,7 @@ const listingSchema = new Schema({
     url: {
       type: String,
       default:
-        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60",
     },
     filename: {
       type: String,
