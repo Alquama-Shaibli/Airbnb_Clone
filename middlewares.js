a13f5c153cdc5000563e0e2410350f17ed5152bc
alquama-shaibli <alquama.r56@gmail.com>
2026-08-10 22:44:40 +0530

    ErrorFixed

--- middlewares.js
+++ middlewares.js
@@ -45,9 +45,9 @@ module.exports.validateListing = (req, res, next) => {
 
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    let {id, reviewId } = req.params;
-    let Review = await Listing.findById(Reviewid);
-  if(!review.author.equals(res.locals.currentUser._id)){ 
+    let { id, reviewId } = req.params;
+    let reviewDoc = await review.findById(reviewId);
+  if(!reviewDoc.author.equals(res.locals.currentUser._id)){ 
     req.flash("error", "You are not the author!");
     return res.redirect(`/listings/${id}`);
   }
