eacd6f574a76a266adb28df4c28d9b1c73d8cda3
alquama-shaibli <alquama.r56@gmail.com>
2026-08-13 23:49:51 +0530

    MulterAdded

--- routes/listing.js
+++ routes/listing.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middlewares.js");
 const listingsController = require("../controllers/listings.js");
-
+const Listing = require("../models/listing.js");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); // Configure multer to store uploaded files in the "uploads" directory
 
 router.route("/")
     .get(wrapAsync(listingsController.index))
     .post(isLoggedIn, validateListing, wrapAsync(listingsController.create));
+    .post( upload.single('Listing[image]'),(req, res) => {
+        // Access the uploaded file through req.file
+        console.log(req.file);
+        res.send('File uploaded successfully!');
+    });
 
 
 
