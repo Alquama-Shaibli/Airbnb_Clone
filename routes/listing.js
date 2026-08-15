b66bf84fd2a135ea92dbc83214515f90161efe7c
alquama-shaibli <alquama.r56@gmail.com>
2026-08-15 20:03:18 +0530

    CloudStorageSaved

--- routes/listing.js
+++ routes/listing.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middlewares.js");
 const listingsController = require("../controllers/listings.js");
-const Listing = require("../models/listing.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" }); // Configure multer to store uploaded files in the "uploads" directory
+const { storage } = require("../cloudConfig.js");
+const uploadCloud = multer({ storage });
 
 router.route("/")
     .get(wrapAsync(listingsController.index))
-    .post(isLoggedIn, validateListing, wrapAsync(listingsController.create));
-    .post( upload.single('Listing[image]'),(req, res) => {
-        // Access the uploaded file through req.file
-        console.log(req.file);
-        res.send('File uploaded successfully!');
-    });
+    .post(isLoggedIn, uploadCloud.single("listing[image]"), validateListing, wrapAsync(listingsController.create));
 
 
 
@@ -22,7 +17,7 @@ router.get("/new", isLoggedIn, listingsController.newRoute);
 
 router.route("/:id")
     .get(wrapAsync(listingsController.show))
-    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingsController.update))
+    .put(isLoggedIn, isOwner, uploadCloud.single("listing[image]"), validateListing, wrapAsync(listingsController.update))
     .delete(isLoggedIn, isOwner, wrapAsync(listingsController.destroy));
 
 
