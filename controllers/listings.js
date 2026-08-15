b1e553741afcc723856635ff8047f0b8c03f0d26
alquama-shaibli <alquama.r56@gmail.com>
2026-08-15 20:13:25 +0530

    SchemaUpdate

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,19 +1,27 @@
 const Listing = require("../models/listing");
 
-// Index Route
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
 };
 
-// New Route
 module.exports.newRoute = (req, res) => {
   res.render("listings/new.ejs");
 };
 
-// Show Route
+module.exports.create = async (req, res) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  newListing.image = { url, filename };
+  await newListing.save();
+  req.flash("success", "New listing created successfully!");
+  res.redirect("/listings");
+};
+
 module.exports.show = async (req, res) => {
-  let { id } = req.params;
+  const { id } = req.params;
   const listing = await Listing.findById(id)
     .populate({ path: "reviews", populate: { path: "author" } })
     .populate("owner");
@@ -21,22 +29,11 @@ module.exports.show = async (req, res) => {
     req.flash("error", "Listing not found!");
     return res.redirect("/listings");
   }
-  console.log(listing);
   res.render("listings/show.ejs", { listing });
 };
 
-// Create Route
-module.exports.create = async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  newListing.owner = req.user._id; // Assign the logged-in user as the owner of the listing
-  await newListing.save();
-  req.flash("success", "Successfully made a new listing!");
-  res.redirect("/listings");
-};
-
-// Edit Route
 module.exports.edit = async (req, res) => {
-  let { id } = req.params;
+  const { id } = req.params;
   const listing = await Listing.findById(id);
   if (!listing) {
     req.flash("error", "Listing not found!");
@@ -45,19 +42,20 @@ module.exports.edit = async (req, res) => {
   res.render("listings/edit.ejs", { listing });
 };
 
-// Update Route
 module.exports.update = async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  req.flash("success", "Successfully updated the listing!");
+  const { id } = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (req.file) {
+    listing.image = { url: req.file.path, filename: req.file.filename };
+    await listing.save();
+  }
+  req.flash("success", "Listing updated successfully!");
   res.redirect(`/listings/${id}`);
 };
 
-// Delete Route
 module.exports.destroy = async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
   req.flash("success", "Listing deleted successfully!");
-  console.log(deletedListing);
   res.redirect("/listings");
 };
