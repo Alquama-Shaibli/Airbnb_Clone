952b7b6c1b42e645fa67681f60fb11e714cb3dbe
alquama-shaibli <alquama.r56@gmail.com>
2026-08-20 23:37:56 +0530

    bugFixed

--- models/listing.js
+++ models/listing.js
@@ -9,15 +9,9 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    url: {
-      type: String,
-      default:
-        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?auto=format&fit=crop&w=800&q=60",
-    },
-    filename: {
-      type: String,
-      default: "listingimage",
-    },
+    url: String,
+    filename: String,
+     
   },
   price: Number,
   location: String,
