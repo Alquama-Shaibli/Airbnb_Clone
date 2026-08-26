bed4e56f0fc99578ed46e614be037ece82c3e632
alquama-shaibli <alquama.r56@gmail.com>
2026-08-26 21:19:57 +0530

    AddedMarker

--- public/js/map.js
+++ public/js/map.js
@@ -5,7 +5,10 @@
     const map = new mapboxgl.Map({
         accessToken: 'YOUR_MAPBOX_ACCESS_TOKEN',
         container: 'map',
-        center: [77.209, 28.6139], // Default center coordinates (longitude, latitude)
+        center: coordinates, // Default center coordinates (longitude, latitude)
         zoom: 9
     });
  
+    const marker = new mapboxgl.Marker({color: 'red'})
+        .setLngLat([coordinates]) // Default marker coordinates (longitude, latitude)
+        .addTo(map);
