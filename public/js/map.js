cd3d39a0de27caa4412d10cf17fd55c711175bc6
alquama-shaibli <alquama.r56@gmail.com>
2026-08-26 21:25:42 +0530

    PopUpAddedonMarker

--- public/js/map.js
+++ public/js/map.js
@@ -11,4 +11,6 @@
  
     const marker = new mapboxgl.Marker({color: 'red'})
         .setLngLat([coordinates]) // Default marker coordinates (longitude, latitude)
+        .setPopup(new mapboxgl.Popup({offset: 25})
+        .setHTML(`<h3>${listing.title}</h3><p>Location may vary</p>`))
         .addTo(map);
